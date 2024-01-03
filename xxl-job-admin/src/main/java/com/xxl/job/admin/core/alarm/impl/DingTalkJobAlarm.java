3f30fb85df51b5d4e5ad876653c821cbc9b595fd
MinLancer <xieyaoqinghao@163.com>
2024-01-03 14:34:04 +0800

    钉钉告警

--- xxl-job-admin/src/main/java/com/xxl/job/admin/core/alarm/impl/DingTalkJobAlarm.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/alarm/impl/DingTalkJobAlarm.java
@@ -1,8 +1,22 @@
 package com.xxl.job.admin.core.alarm.impl;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
 import com.xxl.job.admin.core.alarm.JobAlarm;
+import com.xxl.job.admin.core.conf.XxlJobAdminConfig;
+import com.xxl.job.admin.core.model.XxlJobGroup;
 import com.xxl.job.admin.core.model.XxlJobInfo;
 import com.xxl.job.admin.core.model.XxlJobLog;
+import com.xxl.job.admin.core.util.DingtalkRobotUtils;
+import com.xxl.job.core.biz.model.ReturnT;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 /**
  * job alarm by DingTalk
@@ -11,9 +25,120 @@ import com.xxl.job.admin.core.model.XxlJobLog;
  * @projectName xxl-job-dingtalk
  * @date 2024/1/2 10:47
  */
+@Component
 public class DingTalkJobAlarm implements JobAlarm {
+
+    private final static Logger logger = LoggerFactory.getLogger(DingTalkJobAlarm.class);
+
+    /** 钉钉 定时任务通知群 **/
+    private DingtalkRobotUtils dingtalkRobotUtils;
+
+    /** 钉钉 报表通知群 **/
+    private DingtalkRobotUtils dingtalkRobotUtilsToStatis;
+
+    @Value("${dingtalk.robot.schedulejob.enabled}")
+    private Boolean schedulejobEnable;
+
+    @Value("${dingtalk.robot.schedulejob.token}")
+    private String schedulejobToken;
+
+    @Value("${dingtalk.robot.schedulejob.secret}")
+    private String schedulejobSecret;
+
+    @Value("${dingtalk.robot.schedulejob.include}")
+    private String schedulejobInclude;
+
+    @Value("${dingtalk.robot.statis.enabled}")
+    private Boolean statisEnable;
+
+    @Value("${dingtalk.robot.statis.token}")
+    private String statisToken;
+
+    @Value("${dingtalk.robot.statis.secret}")
+    private String statisSecret;
+
+    @Value("${dingtalk.robot.statis.include}")
+    private String statisInclude;
+
     @Override
     public boolean doAlarm(XxlJobInfo info, XxlJobLog jobLog) {
-        return false;
+        if(info == null){
+            return true;
+        }
+
+        logger.info("jobLog.ExecutorParam = {}",jobLog.getExecutorParam());
+
+        // 告警内容 、类型
+        String alarmContent = null,faileType = null;
+        if (jobLog.getTriggerCode() != ReturnT.SUCCESS_CODE) {
+            faileType = "调度失败";
+            alarmContent = jobLog.getTriggerMsg();
+        }
+        if (jobLog.getHandleCode()>0 && jobLog.getHandleCode() != ReturnT.SUCCESS_CODE) {
+            faileType = "执行失败";
+            alarmContent = jobLog.getHandleMsg();
+        }
+
+        //不发告警通知的，保存日志
+        if(Boolean.FALSE.equals(schedulejobEnable) && Boolean.FALSE.equals(statisEnable)){
+            logger.error("任务 {} {} 失败 ,msg= {}",info.getJobDesc(),faileType,alarmContent);
+            return true;
+        }
+
+        XxlJobGroup group = XxlJobAdminConfig.getAdminConfig().getXxlJobGroupDao().load(info.getJobGroup());
+
+        String text = String.format(DingtalkRobotUtils.TEMPLATE_MARKDOWN_SHEDULEJOB_FAIL
+                ,DingtalkRobotUtils.RED
+                ,"任务执行失败"
+                ,group!=null?group.getTitle():"null"
+                ,info.getId()
+                ,info.getJobDesc()
+                , DateUtil.formatDateTime(jobLog.getTriggerTime())
+                ,jobLog.getId()
+                ,faileType
+                , StrUtil.sub(alarmContent,0,700) );
+
+        //钉钉通知的类型 0=不通知 ，1=任务通知 2=报表通知
+        int dingTalkType = 0;
+        if(JSONUtil.isTypeJSON(jobLog.getExecutorParam())){
+            dingTalkType = JSONUtil.parseObj(jobLog.getExecutorParam()).getInt("dingTalkType",0);
+        }
+
+        if(Boolean.TRUE.equals(schedulejobEnable) && (1 == dingTalkType || StrUtil.contains(schedulejobInclude, info.getJobDesc()))) {
+            dingtalkRobotUtils.sendMessageMarkdown("任务执行结果", text);
+        }
+
+        if(Boolean.TRUE.equals(statisEnable) && (2 == dingTalkType || StrUtil.contains(statisInclude, info.getJobDesc()))) {
+            dingtalkRobotUtilsToStatis.sendMessageMarkdown("报表任务执行结果", text);
+        }
+        return true;
+    }
+
+    @PostConstruct
+    public void init(){
+        logger.info("xxl-job 钉钉机器人 上线啦");
+
+        if(Boolean.TRUE.equals(schedulejobEnable)){
+            dingtalkRobotUtils = new DingtalkRobotUtils(schedulejobToken,schedulejobSecret);
+            dingtalkRobotUtils.sendMessageText("xxl-job 钉钉机器人 上线啦！");
+        }
+
+        if(Boolean.TRUE.equals(statisEnable)){
+            dingtalkRobotUtilsToStatis = new DingtalkRobotUtils(statisToken,statisSecret);
+            dingtalkRobotUtilsToStatis.sendMessageText("xxl-job 报表钉钉机器人 上线啦！");
+        }
+    }
+
+    @PreDestroy
+    public void destroy() {
+        logger.info("xxl-job 钉钉机器人 下线啦");
+
+        if(Boolean.TRUE.equals(schedulejobEnable)) {
+            dingtalkRobotUtils.sendMessageText("xxl-job 钉钉机器人 下线啦！");
+        }
+
+        if(Boolean.TRUE.equals(statisEnable)){
+            dingtalkRobotUtils.sendMessageText("xxl-job 报表钉钉机器人 下线啦！");
+        }
     }
 }
---
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/util/DingtalkRobotUtils.java
@@ -0,0 +1,162 @@
+package com.xxl.job.admin.core.util;
+
+import com.dingtalk.api.DefaultDingTalkClient;
+import com.dingtalk.api.DingTalkClient;
+import com.dingtalk.api.request.OapiRobotSendRequest;
+import com.taobao.api.ApiException;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.CollectionUtils;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+/**
+ * 钉钉机器人工具类
+ * @author Lancer
+ * @title: DingtalkRobotUtils
+ * @projectName xxl-job-dingtalk
+ * @date 2024/1/2 11:00
+ */
+public class DingtalkRobotUtils {
+    private final static Logger logger = LoggerFactory.getLogger(DingtalkRobotUtils.class);
+    private static final String HMAC_SHA256 ="HmacSHA256";
+
+    public static final String RED = "#F25643";
+    public static final String GREEN = "#15BC83";
+    public static final String YELLOW = "#FF9900";
+    public static final String BLUE = "#0089FF";
+
+    public static final String MARKDOWN_TITLE = "# <font color=\"%s\">%s</font>\n ---\n";
+
+    /** 任务执行完毕 **/
+    public static final String TEMPLATE_MARKDOWN_SHEDULEJOB_WASEXECUTED = MARKDOWN_TITLE +
+            "- 任务【%s】执行完成\n"  +
+            "- 耗时：%s\n"  +
+            "- 下一次执行时间：%s";
+
+    /** 任务执行失败 **/
+    public static final String TEMPLATE_MARKDOWN_SHEDULEJOB_FAIL = MARKDOWN_TITLE +
+            "- 执行器【%s】\n"  +
+            "- 任务id：%s\n"  +
+            "- 任务描述：%s\n"  +
+            "- 触发时间 ：%s\n"+
+            "- 日志id ：%s\n"+
+            "- %s ：%s";
+
+    private final String dingTalkToken;
+
+    private final String secret;
+
+    public DingtalkRobotUtils (String dingTalkToken,String secret) {
+        this.dingTalkToken = dingTalkToken;
+        this.secret = secret;
+    }
+
+    /**
+     * 发送text消息
+     * @param messageText
+     */
+    public void sendMessageText(String messageText){
+        sendMessageText(messageText,null);
+    }
+
+    /**
+     * 发送text消息
+     * @param messageText
+     * @param atPhones
+     */
+    public void sendMessageText(String messageText, List<String> atPhones){
+        Long timestamp = System.currentTimeMillis();
+        DingTalkClient client = new DefaultDingTalkClient(String.format("https://oapi.dingtalk.com/robot/send?access_token=%s&timestamp=%d&sign=%s",dingTalkToken,timestamp,sign(timestamp,secret)));
+        OapiRobotSendRequest request = new OapiRobotSendRequest();
+
+        request.setMsgtype("text");
+        OapiRobotSendRequest.Text text = new OapiRobotSendRequest.Text();
+        text.setContent(messageText);
+        request.setText(text);
+
+        if(!CollectionUtils.isEmpty(atPhones)){
+            OapiRobotSendRequest.At at=new OapiRobotSendRequest.At();
+            at.setAtMobiles(atPhones);
+            request.setAt(at);
+        }
+
+        try {
+            client.execute(request);
+        } catch (ApiException e) {
+            logger.error("[ERROR] sendMessage", e);
+        }
+    }
+
+    /**
+     * 发送markdown消息
+     * @param title
+     * @param text
+     */
+    public void sendMessageMarkdown(String title,String text){
+        sendMessageMarkdown(title, text, null);
+    }
+
+    /**
+     * 发送markdown消息
+     * @param title
+     * @param text
+     * @param atPhones
+     */
+    public void sendMessageMarkdown(String title,String text,List<String> atPhones){
+        Long timestamp = System.currentTimeMillis();
+        DingTalkClient client = new DefaultDingTalkClient(String.format("https://oapi.dingtalk.com/robot/send?access_token=%s&timestamp=%d&sign=%s",dingTalkToken,timestamp,sign(timestamp,secret)));
+        OapiRobotSendRequest request = new OapiRobotSendRequest();
+
+        request.setMsgtype("markdown");
+        OapiRobotSendRequest.Markdown markdown = new OapiRobotSendRequest.Markdown();
+        markdown.setTitle(title);
+        markdown.setText(text);
+        request.setMarkdown(markdown);
+
+        if(!CollectionUtils.isEmpty(atPhones)){
+            OapiRobotSendRequest.At at=new OapiRobotSendRequest.At();
+            at.setAtMobiles(atPhones);
+            request.setAt(at);
+        }
+
+        try {
+            client.execute(request);
+        } catch (ApiException e) {
+            logger.error("[ERROR] sendMessage", e);
+        }
+    }
+
+    /**
+     * 签名
+     * @param timestamp
+     * @param secret
+     * @return
+     */
+    static String sign(Long timestamp,String secret){
+        String sign = null;
+        try {
+            String stringToSign = timestamp + "\n" + secret;
+            Mac mac = Mac.getInstance(HMAC_SHA256);
+            mac.init(new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), HMAC_SHA256));
+            byte[] signData = mac.doFinal(stringToSign.getBytes(StandardCharsets.UTF_8));
+            sign = URLEncoder.encode(new String(Base64.encodeBase64(signData)),StandardCharsets.UTF_8.name());
+        } catch (NoSuchAlgorithmException e) {
+            logger.error("sign Mac.getInstance", e);
+        } catch (UnsupportedEncodingException e) {
+            logger.error("sign", e);
+        } catch (InvalidKeyException e) {
+            logger.error("sign mac.init", e);
+        }
+
+        return sign;
+    }
+}
